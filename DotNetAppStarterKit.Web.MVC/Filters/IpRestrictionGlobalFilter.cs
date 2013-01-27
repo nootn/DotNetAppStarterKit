2deaada4c116ee85bc0a0f58672cdb67849923ef
nootn <nootn@hotmail.com>
2013-01-27 12:17:45 +1000

    Adds MVC project and a couple of filters

---
+++ DotNetAppStarterKit.Web.MVC/Filters/IpRestrictionGlobalFilter.cs
@@ -0,0 +1,123 @@
+﻿// /*
+// Copyright (c) 2013 Andrew Newton (http://www.nootn.com.au)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// */
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using DotNetAppStarterKit.Core.Caching;
+using DotNetAppStarterKit.Core.Logging;
+using DotNetAppStarterKit.Web.MVC.Helpers;
+
+namespace DotNetAppStarterKit.Web.MVC.Filters
+{
+    public class IpRestrictionGlobalFilter : IActionFilter
+    {
+        public static Func<string[]> RestrictToIpAddresses =
+            () =>
+            string.IsNullOrWhiteSpace(ConfigurationManager.AppSettings["IpRestriction-IpAddressesCommaSeparated"])
+                ? null
+                : ConfigurationManager.AppSettings["IpRestriction-IpAddressesCommaSeparated"].Split(',');
+
+        public static Func<string[]> RestrictToDnsAlias =
+            () =>
+            string.IsNullOrWhiteSpace(ConfigurationManager.AppSettings["IpRestriction-DnsAliasCommaSeparatedd"])
+                ? null
+                : ConfigurationManager.AppSettings["IpRestriction-DnsAliasCommaSeparated"].Split(',');
+
+        public static Func<int> SuccessfulCacheTimeoutMinutes = () => 10;
+
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+        }
+
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var cacher = DependencyResolver.Current.GetService<ICacheProvider<SuccessfulIpRestrictedClient>>();
+            var logger = DependencyResolver.Current.GetService<ILog<IpRestrictionGlobalFilter>>();
+
+            var restrictToIpAddresses = RestrictToIpAddresses();
+            var restrictToDnsAlias = RestrictToDnsAlias();
+
+            if (restrictToIpAddresses.Any() || restrictToDnsAlias.Any())
+            {
+                var clientIp = IpHelper.GetClientIpAddress(filterContext.HttpContext.Request);
+
+                //Try and cache it so it's only done once per person if successful (don't cache unsuccessful)
+                if (cacher != null && cacher.GetCachedItem(clientIp) != null)
+                {
+                    //they are allowed in because they were before
+                    return;
+                }
+
+                var foundInRestricted = false;
+
+                if (restrictToIpAddresses.Any())
+                {
+                    foundInRestricted = restrictToIpAddresses.Contains(clientIp);
+                }
+
+                var ipDnsMappings = new List<Tuple<string, string>>();
+
+                if (!foundInRestricted)
+                {
+                    var counter = 0;
+                    while (!foundInRestricted && counter < restrictToDnsAlias.Length)
+                    {
+                        var currHost = restrictToDnsAlias[counter];
+
+                        var hostEntry = Dns.GetHostEntry(currHost);
+                        if (hostEntry != null)
+                        {
+                            var ipOfDns = hostEntry.AddressList[0].ToString();
+                            ipDnsMappings.Add(new Tuple<string, string>(currHost, ipOfDns));
+                            if (string.Equals(clientIp, ipOfDns, StringComparison.InvariantCultureIgnoreCase))
+                            {
+                                foundInRestricted = true;
+                            }
+                        }
+
+                        counter++;
+                    }
+                }
+
+                if (!foundInRestricted)
+                {
+                    if (logger != null)
+                    {
+                        logger.Warning("IP {0} was not allowed in", clientIp);
+                    }
+                    filterContext.Result =
+                        new HttpUnauthorizedResult(
+                            "This resource has network restrictions in place, you are not authorized");
+                }
+                else
+                {
+                    if (cacher != null)
+                    {
+                        cacher.AddCachedItem(new SuccessfulIpRestrictedClient(), clientIp,
+                                             SuccessfulCacheTimeoutMinutes());
+                    }
+
+                    if (logger != null)
+                    {
+                        logger.Debug("IP {0} was allowed in", clientIp);
+                    }
+                }
+            }
+        }
+    }
+
+    internal class SuccessfulIpRestrictedClient
+    {
+    }
+}
---
+++ DotNetAppStarterKit.Web.MVC/Helpers/IpHelper.cs
@@ -0,0 +1,68 @@
+﻿// /*
+// Copyright (c) 2013 Andrew Newton (http://www.nootn.com.au)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// */
+
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace DotNetAppStarterKit.Web.MVC.Helpers
+{
+    public static class IpHelper
+    {
+        /// <summary>
+        /// Gets the current client IP address
+        /// (From http://stackoverflow.com/a/7348761)
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public static string GetClientIpAddress(HttpRequestBase request)
+        {
+            var userHostAddress = request.UserHostAddress;
+
+            IPAddress.Parse(userHostAddress);
+
+            var xForwardedFor = request.ServerVariables["X_FORWARDED_FOR"];
+
+            if (string.IsNullOrEmpty(xForwardedFor))
+                return userHostAddress;
+
+            // Get a list of public ip addresses in the X_FORWARDED_FOR variable
+            var publicForwardingIps = xForwardedFor.Split(',').Where(ip => !IsPrivateIpAddress(ip)).ToList();
+
+            // If we found any, return the last one, otherwise return the user host address
+            return publicForwardingIps.Any() ? publicForwardingIps.Last() : userHostAddress;
+        }
+
+        private static bool IsPrivateIpAddress(string ipAddress)
+        {
+            // http://en.wikipedia.org/wiki/Private_network
+            // Private IP Addresses are: 
+            //  24-bit block: 10.0.0.0 through 10.255.255.255
+            //  20-bit block: 172.16.0.0 through 172.31.255.255
+            //  16-bit block: 192.168.0.0 through 192.168.255.255
+            //  Link-local addresses: 169.254.0.0 through 169.254.255.255 (http://en.wikipedia.org/wiki/Link-local_address)
+
+            var ip = IPAddress.Parse(ipAddress);
+            var octets = ip.GetAddressBytes();
+
+            var is24BitBlock = octets[0] == 10;
+            if (is24BitBlock) return true; // Return to prevent further processing
+
+            var is20BitBlock = octets[0] == 172 && octets[1] >= 16 && octets[1] <= 31;
+            if (is20BitBlock) return true; // Return to prevent further processing
+
+            var is16BitBlock = octets[0] == 192 && octets[1] == 168;
+            if (is16BitBlock) return true; // Return to prevent further processing
+
+            var isLinkLocalAddress = octets[0] == 169 && octets[1] == 254;
+            return isLinkLocalAddress;
+        }
+    }
+}
