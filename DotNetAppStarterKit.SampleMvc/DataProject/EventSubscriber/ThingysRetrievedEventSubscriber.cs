2eab2bbf28872d0e6ddc64038df6c3604e75c406
nootn <me@nootn.com.au>
2013-03-19 15:44:45 +1000

    Updates queries to hand off caching to event handler

--- DotNetAppStarterKit.SampleMvc/DataProject/EventSubscriber/ThingysRetrievedEventSubscriber.cs
+++ DotNetAppStarterKit.SampleMvc/DataProject/EventSubscriber/ThingysRetrievedEventSubscriber.cs
@@ -7,46 +7,27 @@
 // 
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // */
+
 using System.Collections.Generic;
-using System.Linq;
 using DotNetAppStarterKit.Core.Caching;
 using DotNetAppStarterKit.Core.Event;
-using DotNetAppStarterKit.Core.Query;
-using DotNetAppStarterKit.SampleMvc.DataProject.Context;
 using DotNetAppStarterKit.SampleMvc.DataProject.Event;
-using DotNetAppStarterKit.SampleMvc.DataProject.Query.Interface;
-using DotNetAppStarterKit.SampleMvc.DataProject.Query.Mappers;
 using DotNetAppStarterKit.SampleMvc.DataProject.Query.QueryDto;
 
-namespace DotNetAppStarterKit.SampleMvc.DataProject.Query
+namespace DotNetAppStarterKit.SampleMvc.DataProject.EventSubscriber
 {
-    public class GetAllThingysQuery : CachedQueryBase<IEnumerable<ThingyQueryDto>>, IGetAllThingysQuery
+    public class ThingysRetrievedEventSubscriber : IEventSubscriber<ThingysRetrievedEvent>
     {
         private readonly ICacheProvider<IEnumerable<ThingyQueryDto>> _cacheProvider;
-        private readonly IEventPublisher<ThingysRetrievedEvent> _publisher;
-        private readonly IDummyDataContext _context;
-        private readonly ThingyToThingyQueryDtoMapper _mapper;
 
-        public GetAllThingysQuery(IDummyDataContext context, ThingyToThingyQueryDtoMapper mapper,
-                                  ICacheProvider<IEnumerable<ThingyQueryDto>> cacheProvider,
-                                  IEventPublisher<ThingysRetrievedEvent> publisher)
+        public ThingysRetrievedEventSubscriber(ICacheProvider<IEnumerable<ThingyQueryDto>> cacheProvider)
         {
-            _context = context;
-            _mapper = mapper;
             _cacheProvider = cacheProvider;
-            _publisher = publisher;
         }
 
-        public override IEnumerable<ThingyQueryDto> ExecuteCached()
+        public void Handle(ThingysRetrievedEvent data)
         {
-            return _cacheProvider.GetCachedItem("");
-        }
-
-        public override IEnumerable<ThingyQueryDto> Execute()
-        {
-            var res = _context.Thingys.OrderBy(_ => _.Name).ToList().Select(item => _mapper.Map(item, new ThingyQueryDto()));
-            _publisher.Publish(new ThingysRetrievedEvent { RetrievedItems = res });
-            return res;
+            _cacheProvider.AddCachedItem(data.RetrievedItems, "", 10);
         }
     }
 }
--- DotNetAppStarterKit.SampleMvc/DataProject/Query/GetThingyQuery.cs
+++ DotNetAppStarterKit.SampleMvc/DataProject/Query/GetThingyQuery.cs
@@ -11,8 +11,10 @@
 using System;
 using System.Linq;
 using DotNetAppStarterKit.Core.Caching;
+using DotNetAppStarterKit.Core.Event;
 using DotNetAppStarterKit.Core.Query;
 using DotNetAppStarterKit.SampleMvc.DataProject.Context;
+using DotNetAppStarterKit.SampleMvc.DataProject.Event;
 using DotNetAppStarterKit.SampleMvc.DataProject.Query.Interface;
 using DotNetAppStarterKit.SampleMvc.DataProject.Query.Mappers;
 using DotNetAppStarterKit.SampleMvc.DataProject.Query.QueryDto;
@@ -22,15 +24,18 @@ namespace DotNetAppStarterKit.SampleMvc.DataProject.Query
     public class GetThingyQuery : CachedQueryBase<Guid, ThingyQueryDto>, IGetThingyQuery
     {
         private readonly ICacheProvider<ThingyQueryDto> _cacheProvider;
+        private readonly IEventPublisher<ThingyRetrievedEvent> _publisher;
         private readonly IDummyDataContext _context;
         private readonly ThingyToThingyQueryDtoMapper _mapper;
 
         public GetThingyQuery(IDummyDataContext context, ThingyToThingyQueryDtoMapper mapper,
-                              ICacheProvider<ThingyQueryDto> cacheProvider)
+                              ICacheProvider<ThingyQueryDto> cacheProvider,
+                              IEventPublisher<ThingyRetrievedEvent> publisher)
         {
             _context = context;
             _mapper = mapper;
             _cacheProvider = cacheProvider;
+            _publisher = publisher;
         }
 
         public override ThingyQueryDto ExecuteCached(Guid model)
@@ -44,7 +49,7 @@ namespace DotNetAppStarterKit.SampleMvc.DataProject.Query
             if (entity != null)
             {
                 var item = _mapper.Map(entity, new ThingyQueryDto());
-                _cacheProvider.AddCachedItem(item, item.Id.ToString(), 10);
+                _publisher.Publish(new ThingyRetrievedEvent { RetrievedItem = item });
                 return item;
             }
             return null;
