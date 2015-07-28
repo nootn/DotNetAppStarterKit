a3e553f8bbe9bf8b65f09f596fa1f316b5c05038
Jack Watts <jack.watts@readify.net>
2015-07-28 16:03:51 +1000

    Added LifetimeScopeAwareTaskFactory to support static DomainEvents.

---
+++ DotNetAppStarterKit.SampleMvc/Application/LifetimeScopeAwareTaskFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace DotNetAppStarterKit.SampleMvc.Application
+{
+    public interface ILifetimeScopeAwareTaskFactory<TResult>
+    {
+        Task<TResult> StartNew(Func<TResult> function);
+    }
+
+    public class LifetimeScopeAwareTaskFactory<TResult> : ILifetimeScopeAwareTaskFactory<TResult>
+    {
+        private readonly ILifetimeScope _lifetimeScope;
+
+        public LifetimeScopeAwareTaskFactory(ILifetimeScope lifetimeScope)
+        {
+            _lifetimeScope = lifetimeScope;
+        }
+
+        public Task<TResult> StartNew(Func<TResult> function)
+        {
+            return Task.Run(() =>
+            {
+                DependencyResolverEventBroker.LifetimeScope = _lifetimeScope;
+                var result = function();
+                DependencyResolverEventBroker.LifetimeScope = null;
+                return result;
+            });
+        }
+    }
+}
